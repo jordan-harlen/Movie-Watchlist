f81155e22376956d7d5e0f355d93764aa0f9e04d
Jordan Harlen <jordan_harlen@hotmail.com>
2023-02-10 17:52:07 +1300

    made migrations and seeds

--- server/db/migrations/20230210044926_movies.js
+++ server/db/migrations/20230210044926_movies.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('movies', (table) => {
+    table.increments('id')
+    table.string('title')
+    table.string('img')
+    table.string('imdb_id')
+    table.boolean('watched').defaultTo(false)
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('movies')
+}
--- server/db/seeds/movies.js
+++ server/db/seeds/movies.js
@@ -1,13 +1,24 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
-};
+  await knex('movies').del()
+  await knex('movies').insert([
+    {
+      id: 1,
+      title: 'The Batman',
+      imdb_id: 'tt1877830',
+      watched: false,
+      img: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg',
+    },
+    {
+      id: 2,
+      title: 'Arrival',
+      imdb_id: 'tt2543164',
+      watched: true,
+      img: 'https://imdb-api.com/images/original/MV5BNGU0NTA2YjctYWNlYy00ZDg1LTg5ZTItZWM3MWZiMDI5OGYzL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDM3ODU2NDM@._V1_Ratio0.7273_AL_.jpg',
+    },
+  ])
+}
